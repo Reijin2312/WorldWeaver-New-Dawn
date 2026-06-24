a629af480f727add8409a4c9a2348be5c097c5c3
Redmi <terminator4444_4@mail.ru>
2026-06-25 02:02:10 +0500

    fixed world creation crash on 26.1.2, fixed game sometimes freeze on loading

--- wover-item-api/src/main/java/org/betterx/wover/item/mixin/item_stack_setup/ItemInputMixin.java
+++ wover-item-api/src/main/java/org/betterx/wover/item/mixin/item_stack_setup/ItemInputMixin.java
@@ -4,6 +4,7 @@ import org.betterx.wover.item.api.ItemStackHelper;
 
 import net.minecraft.commands.arguments.item.ItemInput;
 import net.minecraft.core.Holder;
+import net.minecraft.core.component.DataComponentPatch;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
@@ -13,6 +14,7 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Group;
 
 @Mixin(ItemInput.class)
 public class ItemInputMixin {
@@ -22,9 +24,29 @@ public class ItemInputMixin {
 
     @WrapOperation(
             method = "createItemStack",
-            at = @At(value = "NEW", target = "net/minecraft/world/item/ItemStack")
+            at = @At(value = "NEW", target = "(Lnet/minecraft/core/Holder;I)Lnet/minecraft/world/item/ItemStack;"),
+            require = 0
     )
+    @Group(name = "wover_item_input_item_stack_init", min = 1, max = 1)
     public ItemStack wover_init(Holder<Item> item, int count, Operation<ItemStack> original) {
         return ItemStackHelper.callItemStackSetupIfPossible(original.call(item, count));
+    }
+
+    @WrapOperation(
+            method = "createItemStack",
+            at = @At(
+                    value = "NEW",
+                    target = "(Lnet/minecraft/core/Holder;ILnet/minecraft/core/component/DataComponentPatch;)Lnet/minecraft/world/item/ItemStack;"
+            ),
+            require = 0
+    )
+    @Group(name = "wover_item_input_item_stack_init", min = 1, max = 1)
+    public ItemStack wover_initWithComponents(
+            Holder<Item> item,
+            int count,
+            DataComponentPatch components,
+            Operation<ItemStack> original
+    ) {
+        return ItemStackHelper.callItemStackSetupIfPossible(original.call(item, count, components));
     }
 }
