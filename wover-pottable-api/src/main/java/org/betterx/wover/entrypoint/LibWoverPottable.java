aa58310a7372f39ee1c4c8703196ac4f8832ec8c
Raijin <the_krap7@mail.ru>
2026-07-25 02:12:08 +0500

    Add the pottable datapack registry API
    Adapted from upstream WorldWeaver aa82e4df for New Dawn and NeoForge:
    https://github.com/quiqueck/WorldWeaver/commit/aa82e4dff6c9002c306e85bb7c321076bd5c226d
    
    Required by BetterEnd 1d755548; the incompatible newer BlockTrait convenience layer is omitted.

---
+++ wover-pottable-api/src/main/java/org/betterx/wover/entrypoint/LibWoverPottable.java
@@ -0,0 +1,13 @@
+package org.betterx.wover.entrypoint;
+
+import org.betterx.wover.core.api.ModCore;
+
+import net.neoforged.bus.api.IEventBus;
+
+public class LibWoverPottable {
+    public static final ModCore C = ModCore.create("wover-pottable", "wover");
+
+    public LibWoverPottable(IEventBus modEventBus) {
+        C.registerDatapackListener(modEventBus);
+    }
+}
