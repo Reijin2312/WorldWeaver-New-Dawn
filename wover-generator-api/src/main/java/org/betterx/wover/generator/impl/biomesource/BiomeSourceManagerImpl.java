00a5c52c89070495799e6ce28e80d97ac3fadcf9
Raijin <the_krap7@mail.ru>
2026-08-01 14:00:30 +0500

    bug fixes

--- wover-generator-api/src/main/java/org/betterx/wover/generator/impl/biomesource/BiomeSourceManagerImpl.java
+++ wover-generator-api/src/main/java/org/betterx/wover/generator/impl/biomesource/BiomeSourceManagerImpl.java
@@ -37,10 +37,10 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 import org.jetbrains.annotations.ApiStatus;
 
@@ -150,7 +150,7 @@ public class BiomeSourceManagerImpl {
         return data instanceof WoverBiomeData woverData ? woverData.genChance : defaultChance;
     }
 
-    private static final Map<TagKey<Biome>, Set<Identifier>> EXCLUSIONS = new HashMap<>();
+    private static final Map<TagKey<Biome>, Set<Identifier>> EXCLUSIONS = new ConcurrentHashMap<>();
     private static final Set<Identifier> FABRIC_EXCLUDES = new ResourceLocationSet();
 
     public static void onResourcesLoaded(ResourceManager resourceManager) {
