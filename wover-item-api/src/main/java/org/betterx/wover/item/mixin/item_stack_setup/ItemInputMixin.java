8691773c6c7ae49346444be4d490ffcd321d753b
Redmi <terminator4444_4@mail.ru>
2026-08-05 10:30:45 +0500

    fixed /give command not working

--- wover-item-api/src/main/java/org/betterx/wover/item/mixin/item_stack_setup/ItemInputMixin.java
+++ wover-item-api/src/main/java/org/betterx/wover/item/mixin/item_stack_setup/ItemInputMixin.java
@@ -4,6 +4,7 @@ import org.betterx.wover.item.api.ItemStackHelper;
 
 import net.minecraft.commands.arguments.item.ItemInput;
 import net.minecraft.core.Holder;
+import net.minecraft.core.component.DataComponentPatch;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
@@ -22,9 +23,17 @@ public class ItemInputMixin {
 
     @WrapOperation(
             method = "createItemStack",
-            at = @At(value = "NEW", target = "net/minecraft/world/item/ItemStack")
+            at = @At(
+                    value = "NEW",
+                    target = "(Lnet/minecraft/core/Holder;ILnet/minecraft/core/component/DataComponentPatch;)Lnet/minecraft/world/item/ItemStack;"
+            )
     )
-    public ItemStack wover_init(Holder<Item> item, int count, Operation<ItemStack> original) {
-        return ItemStackHelper.callItemStackSetupIfPossible(original.call(item, count));
+    public ItemStack wover_init(
+            Holder<Item> item,
+            int count,
+            DataComponentPatch components,
+            Operation<ItemStack> original
+    ) {
+        return ItemStackHelper.callItemStackSetupIfPossible(original.call(item, count, components));
     }
 }
